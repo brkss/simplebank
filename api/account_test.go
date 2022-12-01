4062b30145dcfea865e98a4511b3eaec79929827
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-01 02:14:43 +0100

    🧪 unit tests : account : list accounts !

--- api/account_test.go
+++ api/account_test.go
@@ -53,7 +53,7 @@ func TestGetAccount(t *testing.T) {
 			},
 		},
 		{
-			name:      "NotFound",
+			name:      "InternalError",
 			accountID: account.ID,
 			buildStubs: func(store *mockdb.MockStore) {
 				store.EXPECT().
@@ -124,5 +124,121 @@ func requireMatchAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
 	require.NoError(t, err)
 
 	require.Equal(t, account, gotAccount)
+}
+
+func TestListAccounts(t *testing.T) {
+	accounts := createRandomAccounts(10)
+	testCases := []struct {
+		name          string
+		accounts      []db.Account
+		limit         int64
+		offset        int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:     "OK",
+			accounts: accounts,
+			limit:    5,
+			offset:   5,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Eq(db.ListAccountsParams{Limit: 5, Offset: 5})).
+					Times(1).
+					Return(accounts[0:5], nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				checkListAccountResponse(t, recorder.Body, accounts[0:5])
+			},
+		},
+		{
+			name:     "BadRequest",
+			accounts: accounts,
+			limit:    -1,
+			offset:   -1,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+				//checkListAccountResponse(t, recorder.Body, accounts[0:5])
+			},
+		},
+
+		{
+			name:     "InternalServerError",
+			accounts: accounts,
+			limit:    5,
+			offset:   5,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					ListAccounts(gomock.Any(), gomock.Eq(db.ListAccountsParams{Limit: 5, Offset: 5})).
+					Times(1).
+					Return([]db.Account{}, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+				//checkListAccountResponse(t, recorder.Body, accounts[0:5])
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			// build stubs
+			tc.buildStubs(store)
+
+			recorder := httptest.NewRecorder()
+			server := NewServer(store)
+
+			url := fmt.Sprintf("/accounts/%d/%d", tc.limit, tc.offset)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+
+			tc.checkResponse(t, recorder)
+
+			// check response
+		})
+	}
+}
+
+func createRandomAccounts(n int) []db.Account {
+
+	var accounts []db.Account
+
+	for i := 0; i < n; i++ {
+		accounts = append(accounts, db.Account{
+			ID:       utils.RandomInt(0, 1000),
+			Owner:    utils.RandomOwner(),
+			Currency: utils.RandomCurrency(),
+			Balance:  utils.RandomMoney(),
+		})
+	}
+
+	return accounts
+}
+
+func checkListAccountResponse(t *testing.T, body *bytes.Buffer, accounts []db.Account) {
+
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccounts []db.Account
+	err = json.Unmarshal(data, &gotAccounts)
+	require.NoError(t, err)
+
+	for i := 0; i < len(accounts); i++ {
+		require.Equal(t, accounts[i], gotAccounts[i])
+	}
 
 }
